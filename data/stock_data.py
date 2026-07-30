730458e0bbfb0e3aa0b953ef441f56dc333f687c
Vamshi Krishna D <vamshikrishnadasari52@gmail.com>
2026-07-30 21:18:00 +0530

    Fixed yfinance rate limit issue

--- data/stock_data.py
+++ data/stock_data.py
@@ -8,16 +8,30 @@ def get_stock_data(symbol, period):
 
     stock = yf.Ticker(symbol)
 
-    info = stock.info
-
+    # Historical Data
     if period == "1d":
         history = stock.history(
             period="1d",
             interval="5m"
         )
     else:
-        history = stock.history(
-            period=period
-        )
+        history = stock.history(period=period)
+
+    # Fast information
+    fast = stock.fast_info
+
+    # Try to get detailed info
+    try:
+        detailed = stock.info
+    except Exception:
+        detailed = {}
+
+    info = {
+        "longName": detailed.get("longName", symbol),
+        "currentPrice": fast.get("lastPrice"),
+        "previousClose": fast.get("previousClose"),
+        "marketCap": fast.get("marketCap"),
+        "currency": fast.get("currency", "USD")
+    }
 
     return info, history
